2694ea2ef1d12bae5a999bab6bffd4d7827152f5
sandarutharaka <mstpathirana@students.nsbm.ac.lk>
2025-02-10 18:14:38 +0530

    intial commit

---
+++ lib/pages/employee.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Employee extends StatefulWidget {
+  const Employee({super.key});
+
+  @override
+  State<Employee> createState() => _EmployeeState();
+}
+
+class _EmployeeState extends State<Employee> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Employee",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Form",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(
+          left: 20,
+          top: 30,
+          right: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Name",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10.0),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              "Age",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10.0),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
